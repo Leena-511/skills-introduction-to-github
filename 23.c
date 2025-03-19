b23f0a36c6909a4818feaa135ff042c33527f593
Leena-511 <240130510073.cs@gujaratvidyapith.org>
2025-03-19 09:39:58 +0530

    Add files via upload

---
+++ 23.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+	char str1[20], str2[20];
+	printf("enter 1st string:");
+	gets(str1);
+	
+	printf("enter 2nd string:");
+	gets(str2);
+	
+	if(strcmp(str1,str2)==0)
+	printf("string are equal");
+	else
+	printf("strings are not equal");
+}
